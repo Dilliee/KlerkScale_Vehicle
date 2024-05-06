1bfb5acdd0e5838c8546fdc863fe2f452a742fc8
Dilliee <dillonreyneke40@gmail.com>
2024-05-06 23:36:15 +0200

    Upload

---
+++ Klerkscale_Vehicle_/Klerkscale/Klerkscale/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Klerkscale
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAddVehicle = new System.Windows.Forms.Button();
+            this.txtVehicleName = new System.Windows.Forms.TextBox();
+            this.listBoxVehicles = new System.Windows.Forms.ListBox();
+            this.txtWeight = new System.Windows.Forms.TextBox();
+            this.btnAddWeight = new System.Windows.Forms.Button();
+            this.listBoxWeights = new System.Windows.Forms.ListBox();
+            this.btnGenerateReport = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.txtRegistrationDetails = new System.Windows.Forms.TextBox();
+            this.pnlVehicleManagement = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAddVehicle
+            // 
+            this.btnAddVehicle.Location = new System.Drawing.Point(291, 12);
+            this.btnAddVehicle.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddVehicle.Name = "btnAddVehicle";
+            this.btnAddVehicle.Size = new System.Drawing.Size(100, 28);
+            this.btnAddVehicle.TabIndex = 1;
+            this.btnAddVehicle.Text = "Add Vehicle";
+            this.btnAddVehicle.UseVisualStyleBackColor = true;
+            this.btnAddVehicle.Click += new System.EventHandler(this.btnAddVehicle_Click);
+            // 
+            // txtVehicleName
+            // 
+            this.txtVehicleName.Location = new System.Drawing.Point(16, 15);
+            this.txtVehicleName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtVehicleName.Name = "txtVehicleName";
+            this.txtVehicleName.Size = new System.Drawing.Size(265, 22);
+            this.txtVehicleName.TabIndex = 0;
+            // 
+            // listBoxVehicles
+            // 
+            this.listBoxVehicles.FormattingEnabled = true;
+            this.listBoxVehicles.ItemHeight = 16;
+            this.listBoxVehicles.Location = new System.Drawing.Point(16, 49);
+            this.listBoxVehicles.Margin = new System.Windows.Forms.Padding(4);
+            this.listBoxVehicles.Name = "listBoxVehicles";
+            this.listBoxVehicles.Size = new System.Drawing.Size(265, 116);
+            this.listBoxVehicles.TabIndex = 2;
+            this.listBoxVehicles.SelectedIndexChanged += new System.EventHandler(this.listBoxVehicles_SelectedIndexChanged);
+            // 
+            // txtWeight
+            // 
+            this.txtWeight.Location = new System.Drawing.Point(16, 174);
+            this.txtWeight.Margin = new System.Windows.Forms.Padding(4);
+            this.txtWeight.Name = "txtWeight";
+            this.txtWeight.Size = new System.Drawing.Size(265, 22);
+            this.txtWeight.TabIndex = 3;
+            // 
+            // btnAddWeight
+            // 
+            this.btnAddWeight.Location = new System.Drawing.Point(291, 171);
+            this.btnAddWeight.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddWeight.Name = "btnAddWeight";
+            this.btnAddWeight.Size = new System.Drawing.Size(100, 28);
+            this.btnAddWeight.TabIndex = 4;
+            this.btnAddWeight.Text = "Add Weight";
+            this.btnAddWeight.UseVisualStyleBackColor = true;
+            this.btnAddWeight.Click += new System.EventHandler(this.btnAddWeight_Click);
+            // 
+            // listBoxWeights
+            // 
+            this.listBoxWeights.FormattingEnabled = true;
+            this.listBoxWeights.ItemHeight = 16;
+            this.listBoxWeights.Location = new System.Drawing.Point(16, 206);
+            this.listBoxWeights.Margin = new System.Windows.Forms.Padding(4);
+            this.listBoxWeights.Name = "listBoxWeights";
+            this.listBoxWeights.Size = new System.Drawing.Size(265, 116);
+            this.listBoxWeights.TabIndex = 5;
+            // 
+            // btnGenerateReport
+            // 
+            this.btnGenerateReport.Location = new System.Drawing.Point(16, 330);
+            this.btnGenerateReport.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGenerateReport.Name = "btnGenerateReport";
+            this.btnGenerateReport.Size = new System.Drawing.Size(375, 28);
+            this.btnGenerateReport.TabIndex = 6;
+            this.btnGenerateReport.Text = "Generate Report";
+            this.btnGenerateReport.UseVisualStyleBackColor = true;
+            this.btnGenerateReport.Click += new System.EventHandler(this.btnGenerateReport_Click);
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(291, 268);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(100, 28);
+            this.btnRegister.TabIndex = 7;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // txtRegistrationDetails
+            // 
+            this.txtRegistrationDetails.Location = new System.Drawing.Point(12, 297);
+            this.txtRegistrationDetails.Multiline = true;
+            this.txtRegistrationDetails.Name = "txtRegistrationDetails";
+            this.txtRegistrationDetails.Size = new System.Drawing.Size(281, 82);
+            this.txtRegistrationDetails.TabIndex = 8;
+            // 
+            // pnlVehicleManagement
+            // 
+            this.pnlVehicleManagement.Location = new System.Drawing.Point(0, 0);
+            this.pnlVehicleManagement.Name = "pnlVehicleManagement";
+            this.pnlVehicleManagement.Size = new System.Drawing.Size(200, 100);
+            this.pnlVehicleManagement.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(85, 377);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(216, 36);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Register Vehicle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 562);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnGenerateReport);
+            this.Controls.Add(this.listBoxWeights);
+            this.Controls.Add(this.btnAddWeight);
+            this.Controls.Add(this.txtWeight);
+            this.Controls.Add(this.listBoxVehicles);
+            this.Controls.Add(this.txtVehicleName);
+            this.Controls.Add(this.btnAddVehicle);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Vehicle Management System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAddVehicle;
+        private System.Windows.Forms.TextBox txtVehicleName;
+        private System.Windows.Forms.ListBox listBoxVehicles;
+        private System.Windows.Forms.TextBox txtWeight;
+        private System.Windows.Forms.Button btnAddWeight;
+        private System.Windows.Forms.ListBox listBoxWeights;
+        private System.Windows.Forms.Button btnGenerateReport;
+        private Button btnRegister;
+        private TextBox txtRegistrationDetails;
+        private Panel pnlVehicleManagement;
+        private Button button1;
+    }
+}
---
+++ Klerkscale_Vehicle_/Klerkscale/Klerkscale/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Data.SQLite;
+
+
+namespace Klerkscale
+
+
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, List<double>> vehicles = new Dictionary<string, List<double>>();
+        private SQLiteConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Connection string to SQLite database
+            string databasePath = @"C:\Users\Admin\Documents\Klerkscale\Klerkscale\mydatabase.sqlite";
+            string connectionString = $"Data Source={databasePath};Version=3;";
+            connection = new SQLiteConnection(connectionString);
+        }
+
+        private void btnAddVehicle_Click(object sender, EventArgs e)
+        {
+            string vehicleName = txtVehicleName.Text.Trim();
+            if (!string.IsNullOrEmpty(vehicleName) && !vehicles.ContainsKey(vehicleName))
+            {
+                vehicles.Add(vehicleName, new List<double>());
+                listBoxVehicles.Items.Add(vehicleName);
+                txtVehicleName.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Invalid vehicle name or vehicle already exists.");
+            }
+        }
+
+        private void listBoxVehicles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listBoxWeights.Items.Clear();
+            if (listBoxVehicles.SelectedItem != null)
+            {
+                string selectedVehicle = listBoxVehicles.SelectedItem.ToString();
+                if (vehicles.ContainsKey(selectedVehicle))
+                {
+                    foreach (double weight in vehicles[selectedVehicle])
+                    {
+                        listBoxWeights.Items.Add(weight);
+                    }
+                }
+            }
+        }
+
+        private void btnAddWeight_Click(object sender, EventArgs e)
+        {
+            if (listBoxVehicles.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a vehicle first.");
+                return;
+            }
+
+            double weight;
+            if (double.TryParse(txtWeight.Text, out weight))
+            {
+                string selectedVehicle = listBoxVehicles.SelectedItem.ToString();
+                vehicles[selectedVehicle].Add(weight);
+                listBoxWeights.Items.Add(weight);
+                txtWeight.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid weight.");
+            }
+        }
+
+        private void btnGenerateReport_Click(object sender, EventArgs e)
+        {
+            // StringBuilder to build the report
+            System.Text.StringBuilder report = new System.Text.StringBuilder();
+
+            // Header for the report
+            report.AppendLine("Vehicle Weight Report");
+            report.AppendLine("======================");
+
+            // Iterate over each vehicle in the dictionary
+            foreach (var vehicle in vehicles)
+            {
+                report.AppendLine($"Vehicle: {vehicle.Key}");
+                report.AppendLine("Weights:");
+
+                // Calculate total weight for each vehicle
+                double totalWeight = 0;
+
+                // Append each weight to the report
+                foreach (double weight in vehicle.Value)
+                {
+                    report.AppendLine($" - {weight} kg");
+                    totalWeight += weight;
+                }
+
+                // Append total weight
+                report.AppendLine($"Total Weight: {totalWeight} kg");
+                report.AppendLine();  // Adding a blank line for better readability
+            }
+
+            // Display the report in a MessageBox
+            MessageBox.Show(report.ToString(), "Report", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+
+        private void SaveReportToFile(string report)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Text File (*.txt)|*.txt";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.File.WriteAllText(saveFileDialog.FileName, report);
+            }
+        }
+
+
+        
+
+       
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string registrationDetails = txtRegistrationDetails.Text;
+
+            // Insert registration data into SQLite database
+            string insertQuery = "INSERT INTO Registrations (Details) VALUES (@Details)";
+            using (SQLiteCommand cmd = new SQLiteCommand(insertQuery, connection))
+            {
+                cmd.Parameters.AddWithValue("@Details", registrationDetails);
+
+                try
+                {
+                    connection.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Registration added successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error occurred while registering: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+            // Clear the registration details text box after insertion
+            txtRegistrationDetails.Clear();
+        }
+    }
+}
